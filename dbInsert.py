c3b6de733ea702946b05784f2c9a096e72c75f01
philshen <amyfanpti@gmail.com>
2018-11-25 22:54:01 +0800

    Add delete fucn in dbModel.py.

--- dbInsert.py
+++ dbInsert.py
@@ -6,9 +6,10 @@ from pydrive.drive import GoogleDrive
 
 class GDrive_MoneyHunter():
     
-    def __init__(self,list_foldersfiles,db):
+    def __init__(self,list_foldersfiles,db,dbmodel_class):
         self.list_foldersfiles=list_foldersfiles
         self.db=db
+        self.dbmodel_class=dbmodel_class
 
     # bulk insert atonce then commit
     def DB_Insert(self):
@@ -16,7 +17,7 @@ class GDrive_MoneyHunter():
         
         for dic_folderfiles in self.list_foldersfiles:
             try:
-                BulkInsertTest(self.db).test_bulk_insert_mappings(dic_folderfiles['list'])
+                BulkInsertTest(self.db,self.dbmodel_class).test_bulk_insert_mappings(dic_folderfiles['list'])
             except KeyError as error:
                 print(error)
             except Exception as exception:
@@ -24,21 +25,35 @@ class GDrive_MoneyHunter():
 
         self.db.session.commit()
         duration = time.time() - starttime
-        q = self.db.session.query(GoogleDrive_callputjpg)
+        #q = self.db.session.query(GoogleDrive_callputjpg)
+        q = self.db.session.query(self.dbmodel_class)
         print('Insert {} rows into table.'.format(get_count(q)))
         print('SA ORM Bulk insert, using dictionaries- total time: {:.2f} seconds'.format(duration))
+
+    def DB_DeleteAll(self):
+        starttime=time.time()
+
+        num_rows_deleted = DellTest(self.db,self.dbmodel_class).del_all()
+
+        self.db.session.commit()
+        duration = time.time() - starttime
+        print('SA ORM Bulk delete {} row(s)- total time: {:.2f} seconds'.format(num_rows_deleted,duration))
 
 if __name__ == '__main__':
     configPath = 'config.ini'
     localReadConfig = ReadConfig(configPath)
     str_client_credentials = localReadConfig.get_flask_sqlalchemy_setting('client_credentials')
     parent_id = localReadConfig.get_flask_sqlalchemy_setting('parent_id_folder')
-        
+
+    '''Delete all rows first.'''
+    GDrive_MoneyHunter([],db,GoogleDrive_callputjpg).DB_DeleteAll()
+
     str_candlestick_filepath = ''#
     localgoogle_drive = GoogleCloudDrive(str_candlestick_filepath)
     gauth = localgoogle_drive.GDriveAuth(str_client_credentials)
     drive = GoogleDrive(gauth)
 
     list_foldersfiles=localgoogle_drive.listfolder(drive,parent_id)
-        
-    GDrive_MoneyHunter(list_foldersfiles,db).DB_Insert()
+    '''Then insert data into table.'''    
+    #GDrive_MoneyHunter(list_foldersfiles,db).DB_Insert()
+    GDrive_MoneyHunter(list_foldersfiles,db,GoogleDrive_callputjpg).DB_Insert()
--- dbModel.py
+++ dbModel.py
@@ -1,3 +1,5 @@
+#2018/11/25 Add class DellTest():
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
@@ -41,14 +43,16 @@ class GoogleDrive_callputjpg(db.Model):
         self.modifiedDate = modifiedDate
 
 class BulkInsertTest():
-    def __init__(self,db):
+    def __init__(self,db,dbmodel_class):
         self.db = db
+        self.dbmodel_class = dbmodel_class
 
     """Batched INSERT statements via the ORM in "bulk", discarding PKs."""
     def test_bulk_save(self,list_folderfiles):
         
         self.db.session.bulk_save_objects([
-                GoogleDrive_callputjpg(
+                #GoogleDrive_callputjpg(
+                self.dbmodel_class(    
                 title=dic_files['title'],
                 title1=dic_files['title1'],
                 modifiedDate=dic_files['modifiedDate']
@@ -60,10 +64,41 @@ class BulkInsertTest():
     """Batched INSERT statements via the ORM "bulk", using dictionaries."""
     def test_bulk_insert_mappings(self,list_folderfiles):
 
-        self.db.session.bulk_insert_mappings(GoogleDrive_callputjpg, [
+        self.db.session.bulk_insert_mappings(self.dbmodel_class, [#GoogleDrive_callputjpg, [
                     dic_files
                     for dic_files in list_folderfiles 
         ])        
+
+'''***7.3  Using SQLAlchemy ORM (Object-Relational Mapper)***
+http://www.ntu.edu.sg/home/ehchua/programming/webprogramming/Python3_Flask.html#zz-7.
+****Flask-SQLAlchemy how to delete all rows in a single table***
+https://stackoverflow.com/questions/16573802/flask-sqlalchemy-how-to-delete-all-rows-in-a-single-table'''
+class DellTest():
+    def __init__(self,db,dbmodel_class):
+        self.db = db
+        self.dbmodel_class = dbmodel_class
+    
+    def del_all(self):
+        try:
+            num_rows_deleted = self.db.session.query(self.dbmodel_class).delete()
+            #self.db.session.commit()
+        except:
+            self.db.session.rollback()
+
+        return num_rows_deleted    
+
+    def del_filterby_title(self,str_title):
+        try:
+            '''#for specific value
+                db.session.query(Model).filter(Model.id==123).delete()'''
+            instances_to_delete = self.db.session.query(self.dbmodel_class).filter_by(title=str_title).all()
+            for instance in instances_to_delete:
+                self.db.session.delete(instance)
+
+            #self.db.session.commit()
+        except:
+            db.session.rollback()    
+
 
 #2018/11/22 Fast SQLAlchemy counting (avoid query.count() subquery) 
 #           https://gist.github.com/hest/8798884
